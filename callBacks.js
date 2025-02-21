c6ee04c7f558579db892febfa9dd23de3ec9fd96
Punith <apunithnlg19@gmail.com>
2025-02-21 23:46:19 +0530

    lab-3-11-1-25

---
+++ callBacks.js
@@ -0,0 +1,27 @@
+// Function to simulate fetching the first data
+function getData1(callback) {
+    setTimeout(() => {
+      const data1 = { name: "Alice", age: 25 };
+      callback(data1); // Pass the data to the callback
+    }, 1000); // Simulated delay of 1 second
+  }
+  
+  // Function to simulate fetching the second data
+  function getData2(callback) {
+    setTimeout(() => {
+      const data2 = { city: "New York", country: "USA" };
+      callback(data2); // Pass the data to the callback
+    }, 1500); // Simulated delay of 1.5 seconds
+  }
+  
+    getData1(function(data1) {
+      console.log("Received Data1:", data1);
+      
+      // After Data1 is fetched, call getData2
+      getData2(function(data2) {
+        console.log("Received Data2:", data2);
+
+      });
+    });
+  
+  
---
+++ promises.js
@@ -0,0 +1,27 @@
+function getData1() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const data1 = { name: "Alice", age: 25 };
+        resolve(data1);
+      }, 1000);
+    });
+  }
+    function getData2() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const data2 = { city: "New York", country: "USA" };
+        resolve(data2);
+      }, 1500); 
+    });
+  }
+  getData1()
+    .then((data1) => {
+      console.log("Received Data1:", data1);
+        return getData2();    })
+    .then((data2) => {
+      console.log("Received Data2:", data2);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+  
